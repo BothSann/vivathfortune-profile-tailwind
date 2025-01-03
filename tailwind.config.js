b799d288000877aa29d6fa87c0ad89be30a92b63
BothSann <boththann76@gmail.com>
2025-01-03 23:10:09 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        worksans: ["Work Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        anton: ["Anton", "sans-serif"],
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
